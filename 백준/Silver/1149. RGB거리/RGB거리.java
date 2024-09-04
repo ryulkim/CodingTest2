ba63c6bce8a0cff2abe2e8c7c8bde53e02d57d97
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-09-04 16:38:37 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 18380 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -7,7 +7,7 @@ import java.util.*;
  */
 public class Main {
 	
-	public static int N;
+	public static int N,num;
 	public static int[][] dp;
 	
 	public static void main(String[] args) throws Exception {
@@ -19,7 +19,7 @@ public class Main {
 		for (int i = 0; i < N; i++) {
 			st=new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++) {
-				int num=Integer.parseInt(st.nextToken());
+				num=Integer.parseInt(st.nextToken());
 				dp[i][j]=num;
 				if(i==0) continue;
 				dp[i][j]=Math.min(dp[i-1][(j+1)%3]+num,dp[i-1][(j+2)%3]+num);
