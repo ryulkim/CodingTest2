e53807db7b741bed82e5da438497665419c39b44
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-09-17 23:46:40 +0900

    [Gold IV] Title: N으로 만들기, Time: 140 ms, Memory: 16540 KB -BaekjoonHub

--- 백준/Gold/17255. N으로 만들기/N으로 만들기.java
+++ 백준/Gold/17255. N으로 만들기/N으로 만들기.java
@@ -4,14 +4,12 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 /*
- * 시간: 872ms	메모리: 83,760KB
+ * 시간: 144ms	메모리: 16,560KB
  */
 public class Main {
 	
 	static String s;
 	static int N, ans=0;
-	static HashMap<String, Integer> num; //키: 문자열, 값: 개수 
-//	static HashSet<String> visited;
 	static HashSet<ArrayList<String>> visited;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -19,13 +17,10 @@ public class Main {
 		StringBuilder sb=new StringBuilder();
 		s=br.readLine();
 		N=s.length();
-		num=new HashMap<>();
 		visited=new HashSet<>();
 		
 		for (int i = 0; i < N; i++) {
 			String temp=s.substring(i, i+1);
-			num.put(temp, num.getOrDefault(temp, 0)+1);
-//			visited=new HashSet<>();
 			ArrayList<String> arr=new ArrayList<>();
 			arr.add(temp);
 			dfs(i,i,temp,1,arr);
@@ -45,14 +40,12 @@ public class Main {
 		
 		if(last+1<N) {
 			String temp=str+s.charAt(last+1);
-			num.put(temp, num.getOrDefault(temp, 0)+1);
 			ArrayList<String> arr=new ArrayList<>(path);
 			arr.add(temp);
 			dfs(first, last+1, temp, cnt+1, arr);
 		}
 		if(first-1>=0) {
 			String temp=s.charAt(first-1)+str;
-			num.put(temp, num.getOrDefault(temp, 0)+1);
 			ArrayList<String> arr=new ArrayList<>(path);
 			arr.add(temp);
 			dfs(first-1, last, temp, cnt+1, arr);
