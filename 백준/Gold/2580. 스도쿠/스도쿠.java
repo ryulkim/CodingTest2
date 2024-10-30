c253fb1e3c17ccbb7b864bc8acc33d663d1576ad
ryulkim <58160874+ryulkim@users.noreply.github.com>
2024-10-30 10:01:15 +0900

    [Gold IV] Title: 스도쿠, Time: 356 ms, Memory: 16404 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -1,138 +1,84 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-
-/* 시간: 568ms  메모리: 34888kb */
+import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	public static int[][] board, select;
-	public static List<int[]> blank;
-	public static int[] range = {0,3,6,9};
-	public static boolean flag=false;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb=new StringBuilder();
-		board=new int[9][9];
-		blank=new ArrayList<int[]>();
-		
-		for (int i = 0; i < 9; i++) {
-			StringTokenizer st=new StringTokenizer(br.readLine());
-			for (int j = 0; j < 9; j++) {
-				board[i][j]=Integer.parseInt(st.nextToken());
-				if(board[i][j]==0) {
-					blank.add(new int[] {i,j});
-				}
-			}
-		}
-		
-		proc(0);
-		
-//		for (int i = 0; i < 9; i++) {
-//			for (int j = 0; j < 9; j++) {
-//				System.out.print(board[i][j]+" ");
-//			}
-//			System.out.println();
-//		}
-		
-		
-	}
-
-	public static void proc(int depth) {
-		if(depth==blank.size()) {
-			if(flag) return;
-			for (int i = 0; i < 9; i++) {
-				for (int j = 0; j < 9; j++) {
-					System.out.print(board[i][j]+" ");
-				}
-				System.out.println();
-			}
-			flag=true;
-			return;
-		}
-		
-		int r=blank.get(depth)[0];
-		int c=blank.get(depth)[1];
-		
-		for (int num = 1; num <= 9; num++) {
-			board[r][c]=num;
-			if(chk(r,c)) {
-				proc(depth+1);
-			}
-		}
-		board[r][c]=0;
-	}
-	
-	public static boolean chk(int r, int c) {
-		boolean number[]=new boolean[10];
-		//가로 췌크
-		for (int i = 0; i < 9; i++) {
-			if(board[r][i]==0) continue;
-			if(number[board[r][i]]) {
-				return false;
-			}
-			number[board[r][i]]=true;
-		}
-		number=new boolean[10];
-		//세로 췌크
-		for (int i = 0; i < 9; i++) {
-			if(board[i][c]==0) continue;
-			if(number[board[i][c]]) {
-				return false;
-			}
-			number[board[i][c]]=true;
-		}
-		//네모 췌크
-		if(!nemoChk(r, c)) return false;
-		
-		return true;
-	}
-	
-	public static boolean nemoChk(int r, int c) {
-		int fromR=0, toR=0, fromC=0, toC=0;
-		boolean number[]=new boolean[10];
-		
-		for (int i = 0; i < 3; i++) {
-			if(r>=range[i]&&r<range[i+1]) {
-				fromR=range[i];
-				toR=range[i+1];
-			}
-			if(c>=range[i]&&c<range[i+1]) {
-				fromC=range[i];
-				toC=range[i+1];
-			}
-		}
-		
-		for (int i = fromR; i < toR; i++) {
-			for (int j = fromC; j < toC; j++) {
-				if(board[i][j]==0) continue;
-				if(number[board[i][j]]) {
-					return false;
-				}
-				number[board[i][j]]=true;
-			}
-		}
-		
-		return true;
-	}
-	
-}
-
-
-class Pos{
-	int i, j;
-
-	public Pos(int i, int j) {
-		super();
-		this.i = i;
-		this.j = j;
-	}
-	
+    static List<int[]> empty;
+    static int[][] map;
+    
+    public static void main(String[] args) throws IOException {
+        input();
+        makeSudoku(0);
+    }
+    
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        map = new int[9][9];
+        empty = new ArrayList<>(20);
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < 9; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                
+                if (map[i][j] == 0) empty.add(new int[] {i, j});
+            }
+        }
+    }
+    
+    static void makeSudoku(int idx) {
+        if (idx == empty.size()) {
+            StringBuilder sb = new StringBuilder();
+            for (int[] arr : map) {
+                for (int num : arr) {
+                    sb.append(num).append(' ');
+                }
+                sb.append('\n');
+            }
+            System.out.println(sb);
+            
+            System.exit(0);
+        }
+        
+        int[] pos = empty.get(idx);
+        for (int i = 1; i <= 9; i++) {
+            if (checkHorizonDup(pos[0], i) && checkVerticalDup(pos[1], i) && checkSquareDup(getStartIdx(pos[0]), getStartIdx(pos[1]), i)) {
+                map[pos[0]][pos[1]] = i;
+                makeSudoku(idx + 1);
+                map[pos[0]][pos[1]] = 0;
+            }
+        }
+    }
+    
+    static boolean checkHorizonDup(int r, int value) {
+        for (int i = 0; i < 9; i++) {
+            if (map[r][i] == value) return false;
+        }
+        
+        return true;
+    }
+    
+    static boolean checkVerticalDup(int c, int value) {
+        for (int i = 0; i < 9; i++) {
+            if (map[i][c] == value) return false;
+        }
+        
+        return true;
+    }
+    
+    static boolean checkSquareDup(int r, int c, int value) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (map[r + i][c + j] == value) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    static int getStartIdx(int num) { 
+        if (num < 3) return 0;
+        else if (num < 6) return 3;
+        else return 6;
+    }
 }
