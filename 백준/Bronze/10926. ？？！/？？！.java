7f00ec14981400d86030a75e39bb960fab3b12c3
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-04-19 00:00:22 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int[][] arr;
+    static int N, M, K;
+    static int ans=0;
+    static ArrayList<int[]> persons;
+    static int deleted;
+    static int[] exit;
+
+    public static void main(String[] args) throws Exception {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	System.out.println(br.readLine()+"??!");
+    }
+}
