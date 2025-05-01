38e5d93a1bb58e24f1265a0e2be5f7a669ec6360
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-05-01 14:40:44 +0900

    [Gold II] Title: 피보나치 수 6, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -3,47 +3,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	static long N;
-	static long[][] BASIC={{1,1},{1,0}};
-	static final int MOD=1000_000_007;
-	
-    public static void main(String[] args) throws NumberFormatException, IOException {
-    	
-    	init();
-    	
-    	long[][] ans=proc(N);
-    	System.out.println(ans[0][1]);
-        
-    }
-    
-    public static void init() throws IOException {
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    	N=Long.parseLong(br.readLine());
-    }
-    
-    public static long[][] proc(long n) {
-    	if(n==1) return BASIC;
-    	
-    	long[][] half=proc(n/2);
-    	long[][] ret=multiply(half, half);
-    	
-    	if(n%2==1) {
-    		ret=multiply(ret, BASIC);
-    	}
-    	
-    	return ret;
-    }
-    
-    public static long[][] multiply(long[][] a, long[][] b) {
-    	long[][] res = new long[2][2];
 
-        res[0][0] = (a[0][0]*b[0][0] + a[0][1]*b[1][0]) % MOD;
-        res[0][1] = (a[0][0]*b[0][1] + a[0][1]*b[1][1]) % MOD;
-        res[1][0] = (a[1][0]*b[0][0] + a[1][1]*b[1][0]) % MOD;
-        res[1][1] = (a[1][0]*b[0][1] + a[1][1]*b[1][1]) % MOD;
+    static final long[][] BASIC = {{1, 1}, {1, 0}};
+    static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        long[][] result = power(N);
+        System.out.println(result[0][1]);
+    }
+
+    public static long[][] power(long n) {
+        if (n == 1) return BASIC;
+
+        long[][] half = power(n / 2);
+        long[][] result = multiply(half, half);
+
+        if (n % 2 == 1) {
+            result = multiply(result, BASIC);
+        }
+
+        return result;
+    }
+
+    public static long[][] multiply(long[][] a, long[][] b) {
+        long[][] res = new long[2][2];
+
+        res[0][0] = (a[0][0] * b[0][0] + a[0][1] * b[1][0]) % MOD;
+        res[0][1] = (a[0][0] * b[0][1] + a[0][1] * b[1][1]) % MOD;
+        res[1][0] = (a[1][0] * b[0][0] + a[1][1] * b[1][0]) % MOD;
+        res[1][1] = (a[1][0] * b[0][1] + a[1][1] * b[1][1]) % MOD;
 
         return res;
     }
-   
 }
