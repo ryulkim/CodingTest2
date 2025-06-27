88d60f1d0fbe40c6599e43540974b8d2b7c6ea38
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-06-27 15:30:58 +0900

    [Silver IV] Title: 수들의 합 2, Time: 96 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static int N, M, ans=0;
+	static int[] arr, sum;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	init();
+    	proc();
+    	System.out.println(ans);
+    }
+    
+    public static void init() throws IOException {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st=new StringTokenizer(br.readLine());
+    	N=Integer.parseInt(st.nextToken());
+    	M=Integer.parseInt(st.nextToken());
+    	arr=new int[N+1];
+    	sum=new int[N+1];
+    	
+    	st=new StringTokenizer(br.readLine());
+    	for (int i = 1; i <= N; i++) {
+			int num=Integer.parseInt(st.nextToken());
+			arr[i]=num;
+			sum[i]=num+sum[i-1];
+		}
+    	
+    }
+
+    public static void proc() {
+    	int f=1, l=1;
+    	
+    	while(l<=N) {
+    		int value=sum[l]-sum[f-1];
+    		if(value==M) {
+//    			System.out.println(f+" "+l);
+    			ans++;
+    			l++;
+    		}
+    		else if(value<M) {
+    			l++;
+    		}
+    		else{
+    			f++;
+    		}
+    	}
+    }
+    
+}
