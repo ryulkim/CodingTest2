b17b3f81a0003cc2327cd9fa271ce52c4188da95
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-07-08 00:10:06 +0900

    [Gold II] Title: 벽 부수고 이동하기 4, Time: 720 ms, Memory: 184316 KB -BaekjoonHub

--- 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
+++ 백준/Gold/16946. 벽 부수고 이동하기 4/벽 부수고 이동하기 4.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
@@ -11,6 +12,7 @@ public class Main {
 	static int[] cnt;
 	static int[] dr= {0,0,1,-1};
 	static int[] dc= {1,-1,0,0};
+	static ArrayDeque<int[]> wall;
 	
     public static void main(String[] args) throws NumberFormatException, IOException {
     	init();
@@ -27,6 +29,7 @@ public class Main {
     	group=new int[N][M];
     	ans=new int[N][M];
     	cnt=new int[1000_000];
+    	wall=new ArrayDeque<>();
     	
     	for (int i = 0; i < N; i++) {
 			String s=br.readLine();
@@ -41,8 +44,9 @@ public class Main {
 				if(arr[i][j]==0&&group[i][j]==0) {
 					group[i][j]=g;
 					cnt[g]++;
-					dfs(g++,i,j);
+					bfs(g++,i,j);
 				}
+				else if(arr[i][j]==1) wall.add(new int[] {i,j});
 			}
 		}
     }
@@ -57,23 +61,24 @@ public class Main {
     }
     
     public static void proc() {
-    	for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if(arr[i][j]==0) continue;
-				int sum=0;
-				HashSet<Integer> g=new HashSet<Integer>();
-				for (int k = 0; k < 4; k++) {
-					int nr=i+dr[k];
-					int nc=j+dc[k];
-					if(!valid(nr,nc)) continue;
-					if(arr[nr][nc]==1) continue;
-					if(g.contains(group[nr][nc])) continue;
-					sum+=cnt[group[nr][nc]];
-					g.add(group[nr][nc]);
-				}
-				ans[i][j]=(sum+1)%10;
+    	while(!wall.isEmpty()) {
+    		int[] info=wall.poll();
+    		int i=info[0];
+    		int j=info[1];
+    		if(arr[i][j]==0) continue;
+			int sum=0;
+			HashSet<Integer> g=new HashSet<Integer>();
+			for (int k = 0; k < 4; k++) {
+				int nr=i+dr[k];
+				int nc=j+dc[k];
+				if(!valid(nr,nc)) continue;
+				if(arr[nr][nc]==1) continue;
+				if(g.contains(group[nr][nc])) continue;
+				sum+=cnt[group[nr][nc]];
+				g.add(group[nr][nc]);
 			}
-		}
+			ans[i][j]=(sum+1)%10;
+    	}
     }
     
     public static void print() {
@@ -87,6 +92,30 @@ public class Main {
     	
     	System.out.println(sb);
     }
+    
+    public static void bfs(int g, int r, int c) {
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{r, c});
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int cr = cur[0], cc = cur[1];
+
+            for (int k = 0; k < 4; k++) {
+                int nr = cr + dr[k];
+                int nc = cc + dc[k];
+
+                if (!valid(nr, nc)) continue;
+                if (arr[nr][nc] == 1) continue;
+                if (group[nr][nc] != 0) continue;
+
+                group[nr][nc] = g;
+                cnt[g]++;
+                q.add(new int[]{nr, nc});
+            }
+        }
+    }
+
     
     public static void dfs(int g, int r, int c) {
     	for (int k = 0; k < 4; k++) {
