3af16e2f4513150d6945dbdde103fe47b7c4559e
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-07-10 18:31:18 +0900

    [Gold V] Title: 탑, Time: 536 ms, Memory: 109028 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,62 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	static int N;
-	static int[] arr, ans;
-	static ArrayDeque<Integer> stack;
-		
-    public static void main(String[] args) throws NumberFormatException, IOException {
-    	init();
-    	proc();
-    	print();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		Deque<Integer> stack = new ArrayDeque<>();
 
-	public static void init() throws NumberFormatException, IOException {
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    	N=Integer.parseInt(br.readLine());
-    	arr=new int[N];
-    	ans=new int[N];
-    	stack=new ArrayDeque<>();
-    	StringTokenizer st=new StringTokenizer(br.readLine());
-    	for (int i = 0; i < N; i++) {
-			arr[i]=Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-    }
-	
-	
-    public static void proc() {
-    	for (int i = 0; i < N; i++) {
-			while(!stack.isEmpty()&&arr[stack.peek()]<arr[i]) {
-				int top=stack.poll();
+
+		for (int i = 0; i < N; i++) {
+			while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+				stack.pop();
 			}
-			
-			if(stack.isEmpty()) {
-				ans[i]=0;
-			}
-			else {
-				ans[i]=stack.peek()+1;
-			}
+			if (stack.isEmpty()) sb.append("0 ");
+			else sb.append(stack.peek() + 1).append(' ');
 			stack.push(i);
-			
 		}
-    }
-    
-    public static void print() {
-    	StringBuilder sb=new StringBuilder();
-    	
-    	for (int i = 0; i < N; i++) {
-			sb.append(ans[i]+" ");
-		}
-    	System.out.println(sb);
-    }
- 
+		System.out.println(sb);
+	}
 }
