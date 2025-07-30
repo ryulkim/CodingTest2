eb25dff77e2a1128ccf656a024ffbcc45d4d0c40
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-07-30 14:28:34 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 640 ms, Memory: 87620 KB -BaekjoonHub

---
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr, min, max;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		init();
+    }
+    
+	public static void init() throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		arr=new int[N];
+		min=new int[4*N];
+		max=new int[4*N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		
+		rmq(0,N-1,0);
+		
+		for (int i = 0; i < M; i++) {
+			st=new StringTokenizer(br.readLine());
+			int a=Integer.parseInt(st.nextToken());
+			int b=Integer.parseInt(st.nextToken());
+			
+			sb.append(query(0, N-1, a-1, b-1, 0)+" "+maxQuery(0, N-1, a-1, b-1, 0)).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+	public static void rmq(int s, int e, int idx) {
+		if(s==e) {
+			min[idx]=arr[s];
+			max[idx]=arr[s];
+			return;
+		}
+		int mid=(s+e)/2;
+		rmq(s, mid, idx*2+1);
+		rmq(mid+1, e, idx*2+2);
+		min[idx]=Math.min(min[idx*2+1], min[idx*2+2]);
+		max[idx]=Math.max(max[idx*2+1], max[idx*2+2]);
+	}
+	
+	public static int query(int s, int e, int ts, int te, int idx) {
+		if(e<ts||te<s) return 1000_000_000;
+		if(ts<=s&&e<=te) {
+			return min[idx];
+		}
+		
+		int mid=(s+e)/2;
+		return Math.min(query(s, mid, ts, te, idx*2+1), query(mid+1, e, ts, te, idx*2+2));
+	}
+	
+	public static int maxQuery(int s, int e, int ts, int te, int idx) {
+		if(e<ts||te<s) return 0;
+		if(ts<=s&&e<=te) {
+			return max[idx];
+		}
+		
+		int mid=(s+e)/2;
+		return Math.max(maxQuery(s, mid, ts, te, idx*2+1), maxQuery(mid+1, e, ts, te, idx*2+2));
+	}
+	
+	public static int update(int s, int e, int target, int value, int idx) {
+		if(s>target||e<target) {
+			return min[idx];
+		}
+		if(s==e) {
+			return min[idx]=value;
+		}
+		int mid=(s+e)/2;
+		return min[idx]=Math.min(update(s, mid, target, value, idx*2+1), update(mid+1, e, target, value, idx*2+2));
+	}
+	
+}
