6783d4dad8ef9e59abeee532a4d3852cc4994429
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-08-18 00:31:04 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 68 ms, Memory: 11472 KB -BaekjoonHub

---
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+
+    public static void main(String[] args) throws Exception {
+        init();
+    }
+
+    public static void init() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < N; i++) {
+			String s=br.readLine();
+			StringBuilder sb = new StringBuilder();
+			
+			for (int j = 0; j < M; j++) {
+				sb.append(s.charAt(j));
+			}
+			
+			sb.reverse();
+			System.out.println(sb);
+		}
+        
+    }
+}
