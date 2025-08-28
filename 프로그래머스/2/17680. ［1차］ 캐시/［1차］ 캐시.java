50d598b3755af2106873c2864874021b87caa40c
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-08-28 19:52:44 +0900

    [level 2] Title: [1차] 캐시, Time: 58.75 ms, Memory: 142 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+class Solution {
+    class Node {
+        String s;
+        int recent;
+        int cnt;
+        boolean isDeleted;
+        
+        Node(String s, int r, int c){
+            this.s=s;
+            this.recent=r;
+            this.cnt=c;
+            this.isDeleted=false;
+        }
+    }
+    
+    PriorityQueue<Node> pq;
+    HashMap<String, Node> hm;
+    int N;
+    
+    public int solution(int cacheSize, String[] cities) {
+        init(cacheSize);
+        return proc(cities);
+    }
+    
+    public int proc(String[] cities){
+        int idx=0;
+        int ans=0;
+        for(String city: cities){
+            String s=city.toLowerCase();
+            Node find=hm.get(s);
+            
+            if(find!=null){
+                // find.recent=idx++;
+                // Node temp=pq.poll();
+                // pq.add(temp);
+                find.isDeleted=true;
+                Node newNode=new Node(s, idx++, 0);
+                pq.add(newNode);
+                hm.put(s,newNode);
+
+                ans++;
+            }
+            else {
+                if(N>0){
+                    while(!pq.isEmpty()&&pq.peek().isDeleted){
+                        pq.poll();
+                    }
+                    
+                    if(hm.size()>=N){
+                        Node minus=pq.poll();
+                        hm.remove(minus.s);
+                    }
+                    Node newNode=new Node(s, idx++, 0);
+                    pq.add(newNode);
+                    hm.put(s,newNode);
+                }
+                
+                ans+=5;
+            }
+            
+        }
+        
+        return ans;
+    }
+    
+    public void init(int sz){
+        pq=new PriorityQueue<>((a,b)->{
+            // return Integer.compare(a.recent, b.recent);
+            if(a.recent==b.recent){
+                return a.s.compareTo(b.s);
+            }
+            return Integer.compare(a.recent, b.recent);
+        });
+        N=sz;
+        hm=new HashMap<>();
+    }
+}
