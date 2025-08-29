6cb8613e814504e7566d70cb792fd38aab1737ee
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-08-30 00:32:55 +0900

    [Silver V] Title: 돌 게임 2, Time: 68 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class Main {
+	
+	static int N, oIdx, tIdx;
+	static ArrayDeque<String> f,l;
+	
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    	init();
+//    	proc();
+    }
+
+	public static void init() throws NumberFormatException, IOException {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	N=Integer.parseInt(br.readLine());
+    	if(N%2==1) {
+    		System.out.println("CY");
+    	}
+    	else {
+    		System.out.println("SK");
+    	}
+    }
+	
+	
+    public static void proc() {
+    	
+
+    }
+    
+ 
+}
