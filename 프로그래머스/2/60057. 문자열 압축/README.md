98f974808b8ca0d3625e285dff78772edcdc438d
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-09-09 14:48:50 +0900

    [level 2] Title: 문자열 압축, Time: 2.72 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 99.1 MB, 시간: 49.88 ms
+메모리: 88.7 MB, 시간: 2.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 09일 13:54:37
+2025년 09월 09일 14:48:47
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -4,92 +4,46 @@ class Solution {
     
     String input;
     int sz;
-    HashMap<String, Integer> hm;
-    
     
     public int solution(String s) {
         input=s;
         sz=s.length();
-        hm=new HashMap<>();
         int answer = 0;
         int min=sz;
         
         for(int i=1;i<=sz;i++){
             int value=proc(i);
-            // System.out.println(i+" "+value);
-            if(value<min){
-                min=value;
-                answer=i;
-            }
+            min=Math.min(min,value);
         }
-        
-        
         return min;
     }
     
-    public int calculateLength(){
-        int length=0;
-        
-        for(Map.Entry<String, Integer> info: hm.entrySet()){
-            String key=info.getKey();
-            String value=info.getValue().toString();
-            
-            System.out.println(key+" "+value);
-            
-            length+=key.length();
-            if(!value.equals("1")){
-                length+=value.length();
-            }
-        }
-        
-        return length;
-    }
-    
     public int proc(int num){
-        StringBuilder sb=new StringBuilder();
         String prev="";
         int cnt=0;
         int length=0;
         
-        for(int i=0;i<sz;i++){
-            if(i%num==0){
-                if(sb!=null&&sb.length()>0){
-                    String s=new String(sb);
-                    if(prev.equals("")){
-                        prev=s;
-                        cnt=1;
-                    }
-                    else if(s.equals(prev)){
-                       cnt++; 
-                    }
-                    else{
-                        if(cnt>1) length+=Integer.toString(cnt).length();
-                        length+=prev.length();
-                        cnt=1;
-                        prev=s;
-                    }
-                    // System.out.println("proc: "+s+" "+cnt+" "+length);
-                }
-                sb=new StringBuilder();
+        for(int i=0;i<sz;){
+            int end=Math.min(sz, i+num);
+            String s=input.substring(i, end);
+            i=end;
+            if(prev.equals("")){
+                prev=s;
+                cnt=1;
             }
-            sb.append(input.charAt(i));
+            else if(s.equals(prev)){
+               cnt++; 
+            }
+            else{
+                if(cnt>1) length+=(int) Math.log10(cnt)+1;
+                length+=prev.length();
+                cnt=1;
+                prev=s;
+            }
         }
-        
-        String s=new String(sb);
-        if(s.equals(prev)){
-           cnt++; 
-        }
-        else{
-            if(cnt>1) length+=Integer.toString(cnt).length();
-            length+=prev.length();
-            cnt=1;
-            prev=s;
-        }
-        // System.out.println("proc: "+s+" "+cnt+" "+length);
         
         if(cnt>1) length+=Integer.toString(cnt).length();
         length+=prev.length();
-        // System.out.println("proc-final: "+s+" "+cnt+" "+length);
         
         return length;
     }
