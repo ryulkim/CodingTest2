43da53a806eee7c3b6c33ecdb76122485f562581
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-10-13 01:19:30 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 64 ms, Memory: 11704 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine()); // 문자열 개수
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            int len = str.length();
+            
+            if (len >= 6 && len <= 9) {
+                sb.append("yes\n");
+            } else {
+                sb.append("no\n");
+            }
+        }
+        
+        System.out.print(sb);
+    }
+}
