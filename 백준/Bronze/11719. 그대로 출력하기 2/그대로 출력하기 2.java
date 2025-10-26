9d00f1e63f81b94bc5c8411c7d251f32ee598567
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-10-27 01:22:37 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 68 ms, Memory: 12112 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] dp;
+
+    public static void main(String[] args) throws IOException {
+    	init();
+//    	proc();
+	}
+    
+    public static void init() throws IOException {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb=new StringBuilder();
+		String s;
+		
+		while((s=br.readLine())!=null) {
+			System.out.println(s);
+		}
+		
+    }
+    
+}
