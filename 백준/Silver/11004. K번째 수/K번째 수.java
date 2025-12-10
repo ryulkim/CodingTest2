a9d4cfec3e04e422aec2bd4108ea1303b3ccf9ac
ryulkim <58160874+ryulkim@users.noreply.github.com>
2025-12-10 16:28:27 +0900

    [Silver V] Title: K번째 수, Time: 1888 ms, Memory: 376552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, K;
+	static int[] arr;
+	
+    public static void main(String[] args) throws IOException {
+    	init();
+	}
+    
+    public static void init() throws IOException {
+    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    	StringTokenizer st=new StringTokenizer(br.readLine());
+    	N=Integer.parseInt(st.nextToken());
+    	K=Integer.parseInt(st.nextToken());
+    	arr=new int[N];
+    	
+    	st=new StringTokenizer(br.readLine());
+    	for (int i = 0; i < N; i++) {
+			arr[i]=Integer.parseInt(st.nextToken());
+		}
+    	
+    	Arrays.sort(arr);
+    	
+    	System.out.println(arr[K-1]);
+    }
+    
+  
+}
