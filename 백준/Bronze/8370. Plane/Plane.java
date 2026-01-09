a95fe80d2ac9e71821a147c41fe7106e9236ec02
ryulkim <58160874+ryulkim@users.noreply.github.com>
2026-01-09 21:14:37 +0900

    [Bronze V] Title: Plane, Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int k1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+        int k2 = Integer.parseInt(st.nextToken());
+
+        int ans = n1 * k1 + n2 * k2;
+        System.out.println(ans);
+    }
+}
