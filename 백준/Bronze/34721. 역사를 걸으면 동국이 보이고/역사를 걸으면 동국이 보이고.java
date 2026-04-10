4472051bbf7be606b8eaf4fead71b3951fc31ea8
ryulkim <58160874+ryulkim@users.noreply.github.com>
2026-04-11 01:07:35 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 84 ms, Memory: 12768 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    static int N;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = Integer.parseInt(sc.next());
+        
+        for(int i=0;i<N;i++){
+            System.out.println("I love DGU");
+        }
+    }
+
+}
